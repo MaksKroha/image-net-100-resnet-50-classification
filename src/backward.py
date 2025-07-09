2d4eb649017947fea1e9168d63496b4436179535
MaksKroha <maxim.krokhmaliuk@gmail.com>
2025-07-09 21:23:27 +0000

    Bugs fixed

--- src/backward.py
+++ src/backward.py
@@ -4,11 +4,10 @@ from src.utils.logger import exception_logger
 
 @exception_logger
 def backward(logits, labels, optimizer, epoch, lb_epsi=0.1, analyzer=None):
+    optimizer.zero_grad()
     loss = CrossEntropyLoss(label_smoothing=lb_epsi)(logits, labels)
     loss.backward()
     optimizer.step()
     if analyzer is not None:
         analyzer.add_train_val(loss.item())
     print(f"loss - {loss.item()}, epoch - {epoch}")
-
-    optimizer.zero_grad()
